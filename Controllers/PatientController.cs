47a38226d52991b5bee9fba14e81035ad0fcc79e
Shou Heng Tan <shou.heng.dev@gmail.com>
2020-07-21 22:10:38 +0800

    initial MVC testing

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Clinic_Management.Models;
+
+namespace Clinic_Management.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient/Random
+        public ActionResult Random()
+        {
+            Patient pat = new Patient() {Name = "Ash"};
+
+            return View(pat);
+        }
+    }
+}
