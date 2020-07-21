25f1359615b023434858faa9dafcc119be446ab0
Shou Heng Tan <shou.heng.dev@gmail.com>
2020-07-21 22:43:15 +0800

    initial commit

---
+++ Clinic Management/Controllers/PatientController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Clinic_Management.Models;
+
+namespace Clinic_Management.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient/Random
+        public ActionResult Random()
+        {
+            var patient = new Patient() { Name = "Ash"};
+
+            return View(patient);
+        }
+    }
+}
